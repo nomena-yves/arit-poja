c6c0ed2c0e2d1a0c76f799a37f5fefc68f55e252
nomena <hei.nomenjanahary@gmail.com>
2026-05-28 22:40:39 +0300

    change the depedence

--- src/main/java/hei/arit/demo/endpoint/rest/controller/health/SubstractController.java
+++ src/main/java/hei/arit/demo/endpoint/rest/controller/health/SubstractController.java
@@ -1,6 +1,6 @@
 package hei.arit.demo.endpoint.rest.controller.health;
 
-import hei.arit.demo.Services.SubstractService;
+import hei.arit.demo.services.SubstractService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/hei/arit/demo/conf/DivideTest.java
+++ src/test/java/hei/arit/demo/conf/DivideTest.java
@@ -2,7 +2,7 @@ package hei.arit.demo.conf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import hei.arit.demo.endpoint.rest.controller.health.DivideController;
+import hei.arit.demo.endpoint.rest.DivideController;
 import hei.arit.demo.services.DivideService;
 import org.junit.jupiter.api.Test;
 
