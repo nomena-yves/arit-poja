b767dbbbbd5b91ec0af540fbd4d4bb02e85378ba
nomena <hei.nomenjanahary@gmail.com>
2026-05-28 17:27:29 +0300

    update the method service

--- src/main/java/hei/arit/demo/Services/ProductService.java
+++ src/main/java/hei/arit/demo/Services/ProductService.java
@@ -1,10 +1,10 @@
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductNumbers {
+public class ProductService {
   public int products(Integer a, Integer b) {
     if (a == null || b == null) {
-      throw new IllegalArgumentExeption("a or b is invalid");
+      throw new IllegalArgumentException("a or b is invalid");
     }
     int result = a * b;
     return result;
