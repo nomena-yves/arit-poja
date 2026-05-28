0a1a81f2a25c012a8f5d06574252c3256ac20c61
nomena <hei.nomenjanahary@gmail.com>
2026-05-28 17:42:22 +0300

    update the dependance

--- src/test/java/hei/arit/demo/conf/SubstractTest.java
+++ src/test/java/hei/arit/demo/conf/SubstractTest.java
@@ -1,8 +1,11 @@
-import org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class SubstractTest {
   SubstractController controller;
 
+  @Test
   void substractTest() {
     assertEquals(2, controller.substractNumbers(5, 3));
   }
