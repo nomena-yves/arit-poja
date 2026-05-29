9966d8dc894f66b6f10052b068f2a27bda8fff42
nomena <hei.nomenjanahary@gmail.com>
2026-05-29 09:23:38 +0300

    add modification

--- src/main/java/hei/arit/demo/endpoint/rest/controller/health/DivideController.java
+++ src/main/java/hei/arit/demo/endpoint/rest/controller/health/DivideController.java
@@ -3,6 +3,7 @@ package hei.arit.demo.endpoint.rest.controller.health;
 import hei.arit.demo.Services.DivideService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class DivideController {
   DivideService service;
 
   @GetMapping("/divid")
-  public double divideDouble(double a, double b) {
+  public double divideDouble(@RequestParam double a, @RequestParam double b) {
     return service.divideNumbers(a, b);
   }
 }
--- src/main/java/hei/arit/demo/endpoint/rest/controller/health/SubstractController.java
+++ src/main/java/hei/arit/demo/endpoint/rest/controller/health/SubstractController.java
@@ -3,6 +3,7 @@ package hei.arit.demo.endpoint.rest.controller.health;
 import hei.arit.demo.Services.SubstractService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class SubstractController {
   SubstractService service;
 
   @GetMapping("/sub")
-  public Long difference(Long a, Long b) {
+  public Long difference(@RequestParam Long a, @RequestParam Long b) {
     return service.substractNumbers(a, b);
   }
 }
